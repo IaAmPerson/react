4664adc6d7663de2077075751aac7d81024d0571
IaAmPerson <52274834+IaAmPerson@users.noreply.github.com>
2019-09-21 15:52:54 +0800

    idecision

--- src/Indecision.js
+++ src/Indecision.js
@@ -4,58 +4,67 @@ import ReactDOM from 'react-dom'
 const Indecision =() => {
     let idata = JSON.parse(localStorage.getItem('choices'))
     let [choices, setChoices] = useState([])
+    let [decision, setDecision] = useState([])
     // let [choice2] = useState([])
-    let handleClick = (e) => {
+    let handleInput = (e) => {
+        setInput(e.target.value)
+    }
+    let handleChoice = (e) => {
         setChoices([
-          ...choices,
-          {
-            choices
-            
-          }
+            setChoices([...choices, input]),
+            setInput('')
         ])
-        let submit = () => {
-            localStorage.setItem('choices',JSON.stringify(choices))
+    let handleDecision = () => {
+            let decision = Math.floor(Math.random() * choices.length)
+            setDecision(choices[decision])
         }
-        let reset = () => {
-            localStorage.setItem('choices',[])
+    let handleReset = () => {
+            setChoices([])
+            setDecision('')
         }
-        // let MakeChoice = () => {
-
-        // }
+        useEffect(() => {
+            localStorage.setItem('choice', JSON.stringify(choices))
+        })
+    
+        }
     }
 
-}
-return(
-<div>
-    <h1>Indecision</h1>
 
-      
-    {
-        choices.map((choice) => {
-            return (
-            <div key={choice}>
-                <p>{choice}</p>
-                </div>
-            
-          
+    return (
+        <div>
+            <h1>Let the computer decide!</h1>
 
-            
-        )})
-    }
-    <textarea col='20' row='40' ></textarea>
-      <br/>
-      <button onClick={submit()}> Submit </button>
-      <button onClick={reset()}> Reset </button>
-      <br/>
-      <button onClick={MakeChoice()}> Make Choice </button>
-    </div>
+            <div className="choice">
+                {choices.length === 0 ? <p>No Choices here!!??!?!?</p> :
+                    <ul>
+
+                        {choices.map((choice) => {
+                            return <li key={choice}>{choice}</li>
+                        })}
+                    </ul>
+                }
+            </div>
+            <p>What are your choices</p>
+            <input onChange={handleInput} type="text" value={input}></input>
+            <br />
+            <button onClick={handleChoice}>Submit</button>
+            <button onClick={handleReset}>Reset</button>
+
+            <div className="decision">
+
+                {
+                    choices.length > 1 ? <button onClick={handleDecision}>Make My Choice!</button> : ''
+                }
+                {decision === '' ? <p></p> : <p>The computer has decided that you shall! <br/>{decision}</p>}
+
+            </div>
+
+
+
+        </div>
+
+
     )
-
-
-
-
-
-
 
 
 ReactDOM.render(<Indecision/>,root)
